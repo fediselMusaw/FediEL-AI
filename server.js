5bae83281c4dca322c58b0ac0102c9b83fa647b3
fediselMusaw <fedisel2@gmail.com>
2026-06-10 22:00:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Sert les fichiers statiques depuis le dossier "public"
-app.use(express.static(path.join(__dirname, "public")));
+// Sert les fichiers statiques (ton site web) directement depuis la racine
+app.use(express.static(__dirname));
 
 // Route pour la page d'accueil
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
